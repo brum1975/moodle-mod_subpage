d86d6a8e61667c6385e97b4d297c90923fda96c8
brum1975 <matthew.cannings@gmail.com>
2013-06-30 16:56:24 +0100

    First attempt at converting latest 2.4 to 2.5
    Fixed use of deprecated functions in renderer.php

--- version.php
+++ version.php
@@ -22,6 +22,6 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$module->version  = 2013052400;
-$module->requires = 2011120100;
+$module->version  = 2013052401;
+$module->requires = 2013051400;
 $module->cron     = 60*60*4; // 4 hours.
